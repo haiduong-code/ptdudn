1fb074b264cdf6cdf7268562c1ee2326c2b34fa8
Nguyễn Hải Dương <23020523@vnu.edu.vn>
2025-09-15 13:07:24 +0700

    Add files via upload

---
+++ ptuddnDemo/src/main/java/com/example/testing_001/PtuddnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtuddnDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtuddnDemoApplication.class, args);
+    }
+
+}
---
+++ ptuddnDemo/src/test/java/com/example/ptuddndemo/PtuddnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ptuddndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtuddnDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
